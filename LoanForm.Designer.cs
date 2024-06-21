e04402eb8c35c9c8d75e64396ef4bf06580f6162
Karl <karl@mail.com>
2024-06-21 11:57:10 +0800

    new

--- LoanForm.Designer.cs
+++ LoanForm.Designer.cs
@@ -30,16 +30,37 @@
         {
             this.components = new System.ComponentModel.Container();
             this.currentClientLabel = new System.Windows.Forms.Label();
-            this.loanTable = new System.Windows.Forms.DataGridView();
             this.loanBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.paidBtn = new System.Windows.Forms.Button();
             this.addLoanBtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.loanTable)).BeginInit();
+            this.scheduleBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.scheduleBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.loanBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.loanBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.loanBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loanBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loanBindingSource2)).BeginInit();
             this.SuspendLayout();
             // 
             // currentClientLabel
             // 
+            this.currentClientLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.currentClientLabel.Dock = System.Windows.Forms.DockStyle.Top;
             this.currentClientLabel.Font = new System.Drawing.Font("Segoe Script", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.currentClientLabel.Location = new System.Drawing.Point(0, 0);
@@ -49,24 +70,6 @@
             this.currentClientLabel.Text = "Loan of Client ID";
             this.currentClientLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.currentClientLabel.Click += new System.EventHandler(this.currentClientLabel_Click);
-            // 
-            // loanTable
-            // 
-            this.loanTable.AllowUserToAddRows = false;
-            this.loanTable.AllowUserToDeleteRows = false;
-            this.loanTable.AutoGenerateColumns = false;
-            this.loanTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.loanTable.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.loanTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.loanTable.DataSource = this.loanBindingSource;
-            this.loanTable.Location = new System.Drawing.Point(16, 59);
-            this.loanTable.Name = "loanTable";
-            this.loanTable.ReadOnly = true;
-            this.loanTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.loanTable.Size = new System.Drawing.Size(1013, 207);
-            this.loanTable.TabIndex = 1;
-            this.loanTable.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.loanTable_CellDoubleClick);
-            this.loanTable.SelectionChanged += new System.EventHandler(this.loanTable_SelectionChanged);
             // 
             // paidBtn
             // 
@@ -92,14 +95,141 @@
             this.addLoanBtn.UseVisualStyleBackColor = false;
             this.addLoanBtn.Click += new System.EventHandler(this.addLoanBtn_Click);
             // 
+            // scheduleBindingSource
+            // 
+            this.scheduleBindingSource.DataSource = typeof(ClientLoanSystem.Entities.Schedule);
+            // 
+            // scheduleBindingSource1
+            // 
+            this.scheduleBindingSource1.DataSource = typeof(ClientLoanSystem.Entities.Schedule);
+            // 
+            // loanBindingSource1
+            // 
+            this.loanBindingSource1.DataSource = typeof(ClientLoanSystem.Entities.Loan);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11,
+            this.dataGridViewTextBoxColumn12});
+            this.dataGridView1.DataSource = this.loanBindingSource2;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1020, 207);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // loanBindingSource2
+            // 
+            this.loanBindingSource2.DataSource = typeof(ClientLoanSystem.Entities.Loan);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "LoanID";
+            this.dataGridViewTextBoxColumn1.HeaderText = "LoanID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "ClientId";
+            this.dataGridViewTextBoxColumn2.HeaderText = "ClientId";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Loan_AMT";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Loan_AMT";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Interest";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Interest";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Term";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Term";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "NoOf_Payment";
+            this.dataGridViewTextBoxColumn6.HeaderText = "NoOf_Payment";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "Deduction";
+            this.dataGridViewTextBoxColumn7.HeaderText = "Deduction";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.DataPropertyName = "Interested_AMT";
+            this.dataGridViewTextBoxColumn8.HeaderText = "Interested_AMT";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.DataPropertyName = "Receivable_AMT";
+            this.dataGridViewTextBoxColumn9.HeaderText = "Receivable_AMT";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.DataPropertyName = "Total_Payable";
+            this.dataGridViewTextBoxColumn10.HeaderText = "Total_Payable";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.DataPropertyName = "Due_Date";
+            this.dataGridViewTextBoxColumn11.HeaderText = "Due_Date";
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            this.dataGridViewTextBoxColumn11.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn12
+            // 
+            this.dataGridViewTextBoxColumn12.DataPropertyName = "Status_Paid";
+            this.dataGridViewTextBoxColumn12.HeaderText = "Status_Paid";
+            this.dataGridViewTextBoxColumn12.Name = "dataGridViewTextBoxColumn12";
+            this.dataGridViewTextBoxColumn12.ReadOnly = true;
+            // 
             // LoanForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(1041, 332);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.addLoanBtn);
             this.Controls.Add(this.paidBtn);
-            this.Controls.Add(this.loanTable);
             this.Controls.Add(this.currentClientLabel);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1057, 371);
@@ -109,8 +239,12 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "LoanForm";
             this.Load += new System.EventHandler(this.LoanForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.loanTable)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.loanBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loanBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loanBindingSource2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -118,7 +252,6 @@
         #endregion
 
         private System.Windows.Forms.Label currentClientLabel;
-        private System.Windows.Forms.DataGridView loanTable;
         private System.Windows.Forms.Button paidBtn;
         private System.Windows.Forms.Button addLoanBtn;
         private System.Windows.Forms.BindingSource loanBindingSource;
@@ -134,5 +267,22 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn totalPayableDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dueDateDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn statusPaidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource scheduleBindingSource;
+        private System.Windows.Forms.BindingSource scheduleBindingSource1;
+        private System.Windows.Forms.BindingSource loanBindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn12;
+        private System.Windows.Forms.BindingSource loanBindingSource2;
     }
 }
